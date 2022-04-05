b028853b44ba8b6fc445230563ce4b66a300ecc1
silverkh <86564376+silverkh@users.noreply.github.com>
2022-04-05 16:29:26 +0900

    Initial commit

---
+++ app/src/main/java/com/example/monthviewactivity/Calendar.java
@@ -0,0 +1,4 @@
+package com.example.monthviewactivity;
+
+public class Calendar {
+}
---
+++ app/src/main/java/com/example/monthviewactivity/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.monthviewactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] items = {"item1", "item2", "item3", "item4", "item5", "item6", "item7", "item8"};
+
+        //어댑터 준비 (배열 객체 이용, simple_list_item_1 리소스 사용
+        ArrayAdapter<String> adapt
+                = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                items);
+
+        // id를 바탕으로 화면 레이아웃에 정의된 GridView 객체 로딩
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        // 어댑터를 GridView 객체에 연결
+        gridview.setAdapter(adapt);
+    }
+}
